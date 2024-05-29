55dd893d4240872caa636a05182eb99241f42669
Natalia <yasnodecor@gmail.com>
2024-05-29 18:20:16 +0300

    make build

---
+++ index.f3b7b323.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.querySelector("#contactForm").addEventListener("submit",function(e){e.preventDefault(),e.target.reset()});
+//# sourceMappingURL=index.f3b7b323.js.map
